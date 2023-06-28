bba42698d7862547c6fd0fdae997022044e08f39
maldr0w <mvrtinstave@gmail.com>
2023-06-28 21:41:35 +0200

    changed original file to precode, added my own uartic file for development

---
+++ uartic.py
@@ -0,0 +1,16 @@
+# Dev: Martin Stave, July 01, 2023 - uartic.py
+# Uartic Project 
+
+import numpy as np
+
+# Defining shipping routes, start and endpoint, latitude and longitude
+shipping_routes = {
+1: [(0.0,0.0), (1.0, 1.0)]
+
+}    
+
+
+
+
+
+
