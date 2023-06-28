783e06881851af0a49a72c7f5413ed33fb7e01cc
gohermgo <107059171+gohermgo@users.noreply.github.com>
2023-06-28 20:09:17 +0200

    uartic py suggested changes to improve user experience

--- Uartic.py
+++ Uartic.py
@@ -47,6 +47,26 @@ for route_name in shipping_routes.keys():
    total_cost += dist * ice_thickness[route_name]
    total_cost += (fuel_cost['a'][0] + fuel_cost['a'][1] * dist)
 
+import emoji
+
+print(emoji.emojize('Python is pretty ok in my book :thumbs_up:'))
+
+for x in range(10):
+  print(emoji.emojize('Wowza... :boy_light_skin_tone:'))
+  sleep(3)
+
+print(emoji.emojize('Ready to give u the number now :check_mark_button:'))
+
+for x in range(10):
+  print('.')
+  sleep(5)
+
+print(emoji.emojize('this is taking a while :confused_face:'))
+
+for x in range(19):
+  print(emoji.emojize('BZZT :control_knobs:'))
+  sleep(1)
+
+print(emoji.emojize('Finally! :drooling_face:'))
 
 print("Total cost:", total_cost)
-
