791290c1685e8df4fdaac45fa5bf2874eff11d16
maldr0w <mvrtinstave@gmail.com>
2023-06-29 12:13:48 +0200

    created noise map, remoted unused dataset

---
+++ noisemap.py
@@ -0,0 +1,26 @@
+
+import noise
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Generating perlin noise map
+shape = (100,100)
+scale = 10.0
+octaves = 6
+persistence = 0.5
+lacunarity = 2.0
+
+world = np.zeros(shape)
+
+for i in range(shape[0]):
+    for j in range(shape[1]):
+        value = noise.pnoise2(i/scale, j/scale, octaves=octaves, persistence=persistence, 
+            lacunarity=lacunarity, repeatx=shape[0], repeaty=shape[1], base=0)
+        mapped_value = np.interp(value, [-1,1], [0.0, 3.0])
+        world[i][j]=mapped_value
+        
+
+
+plt.imshow(world,cmap='gray',interpolation='nearest',vmin=0.0, vmax=3.0)
+plt.colorbar()
+plt.show()
--- uartic.py
+++ uartic.py
@@ -1,16 +1,29 @@
 # Dev: Martin Stave, July 01, 2023 - uartic.py
 # Uartic Project 
 
-import numpy as np
+# Data aquizition
+   
+from netCDF4 import Dataset
 
-# Defining shipping routes, start and endpoint, latitude and longitude
-shipping_routes = {
-1: [(0.0,0.0), (1.0, 1.0)]
+nc_file = Dataset('thk_2011_octnov.map.nc','r') 
 
-}    
+# Finding variable names
+variable_names = list(nc_file.variables.keys())
+for var_name in variable_names:
+    print(var_name)
+
+nc_file.close()
 
 
 
+# Access data varibales
+thickness = nc_file.variables['thickness'] 
+latitude = nc_file.variables['latitude']
+longitude = nc_file.variables['longitude']
+
+grid_spacing=nc_file.variables['grid_spacing']
 
 
-
+# Access data
+thickness = thickness[:]
+print(thickness.type())
