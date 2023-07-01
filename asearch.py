50e6c198ed2ad5fcf373bd6ea5a7d357d5ced699
maldr0w <mvrtinstave@gmail.com>
2023-07-01 14:05:55 +0200

    added cost and reconst functions, renamed a*outline

--- asearch.py
+++ asearch.py
@@ -10,8 +10,30 @@ def heuristic(node,goal):
     return abs(x2 - x1) + abs(y2 - y1)
 
 
+def reconstruct_path(came_from, current_node):
+    path = [current_node] # creating a path putting each visited node in a list
+    while current_node 
+    
+    
+def get_neighbors(node):
+    neighbors = []
+    x, y = node
+    
+    # Adding each neighbor   
+    neighbors.append(x,y+1) # up
+    neighbors.append(x,y-1) # down
+    neighbors.append(x+1,y) # right
+    neighbors.append(x-1,y) # left
+    
+    return neighbors
+
+def cost_between(node1,node2):
+    
+
+
+
 # A* search algorithm
-def a_star_search(start,goal):
+def a_star_search(start, goal):
     open_set = {start}  # create open set, unordered list, will hold potential nodes
     closed_set = set()  # empty set, for explored nodes
     came_from = {}      # store parent node for each node 
@@ -19,9 +41,25 @@ def a_star_search(start,goal):
     fscore = {start: heuristic(start)} # estimated total cost from start to goal via each node
 
     while open_set: # while there are available nodes
-        current_node = min(open_set,key=lambda node: fscore[node]) # set node with smallest fscore from open_set to current
-        
-    
+        current_node = min(open_set, key=lambda node: fscore[node]) # set node with smallest fscore from open_set to current,                                                                  
+                                                                    # min fscore = node with lowest total cost to reach goal
+
+
+
+        # Having found the lowest fscore node:
+         
+        open_set.remove(current_node) # we remove it from set of possible nodes
+        closed_set.add(current_node)  # and add to explored nodes 
+
+
+        for neighbor in get_neighbors(current_node):
+            if neighbor in closed_set:
+                continue # move to next neighbor
+            
+            tentative_gscore = gscore[current_node] + cost_between(current, neighbor) # calculate gscore
+
+
+
 
 
 
