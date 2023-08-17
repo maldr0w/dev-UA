10aab8278bd08572001c391cf49450228c75150d
maldr0w <mvrtinstave@gmail.com>
2023-08-17 13:00:06 +0200

    found land points

--- dev-UA/new_star.py
+++ dev-UA/new_star.py
@@ -7,7 +7,7 @@ import cartopy as cp
 
 ## Extracting dataset variables
 FILE_NAME = 'ice_thickness_2021.nc'
-GRID_RESOLUTION = 25000 # m
+RESOLUTION = 25000 # m
 
 def extract_data(file):
     '''
@@ -58,18 +58,69 @@ def scatter_plot():
     
 # scatterplot()
 
-import geopandas as gpd
+lat,lon = latitude, longitude
+
+
 from shapely.geometry import Point
+from global_land_mask import globe
+
+## Finding which coordinates are on land
+
+# combining latitude and longitude coordinates
+# points = [Point(lat,lon) for lat, lon in zip(np.ravel(lat),np.ravel(lon))]
+
+points = np.array(list(zip(latitude.flatten(), longitude.flatten())))
+# print(points[0][0])
 
 
-land_geometries = [geom for geom in cfeature.LAND.geometries()]
+# add points from the dataset that are on land to a list
+# land_mask = [point for point in points if globe.is_land(point.x,point.y)]
+# print(f'lat={point.x}, lon={point.y} is on land!')
 
-def is_on_land(lon,lat):
-    point = Point(lon,lat)
-    for geom in land_geometries:
-        if geom.contains(point):
-            return True
-    return False
 
-on_land_flags = [is_on_land(lon,lat) for lon,lat in zip(longitude,latitude)]
-print(on_land_flags)
+## Projection
+from pyproj import CRS, Transformer
+from rasterio.transform import from_origin
+# defining coordinate refrence systems
+latlon_crs = CRS(proj='latlong', datum='WGS84')  # lat/lon projection in degrees
+stereographic_crs = CRS('EPSG:32661')  # North Polar Stereographic projection in meters
+
+# initializing transformers for degrees and meter convertion
+transformer_m = Transformer.from_crs(latlon_crs, stereographic_crs)  # degrees to meters
+transformer_d = Transformer.from_crs(stereographic_crs, latlon_crs)  # meters to degrees
+
+# transforming
+lon_m, lat_m = transformer_m.transform(lon, lat)
+
+land_mask_m = []
+for point in land_mask:
+    transformer_m.transform(point.x,point.y)
+    land_mask_m.append(point)
+    
+print(land_mask_m[0]) 
+
+
+# get extent of coordinates and defining grid parameters
+xmin, ymin, xmax, ymax = lon_m.min(), lat_m.min(), lon_m.max() + 1, lat_m.max() + 1
+n_cols, n_rows = int(np.ceil((xmax - xmin) / RESOLUTION)), int(np.ceil((ymax - ymin) / RESOLUTION))
+    
+# initialize raster transform which maps pixel coordinates to geographic coordinates
+raster_transform = from_origin(xmin, ymax, RESOLUTION, RESOLUTION)
+
+# initializing grid with zeros
+ice_thickness_grid = np.zeros((n_rows, n_cols))  # np.array uses (row,col) convention
+
+
+
+# iterating over all points in the 2D ice thickness grid
+for i in range(ice_thickness.shape[0]):
+    for j in range(ice_thickness.shape[1]):
+
+        x, y = lon_m[i,j], lat_m[i,j]  # extracting corresp. geographic coordinate (in meters) for each point
+        col, row = ~raster_transform * (x,y)  # inverse raster transform: transforming geographic coordinates to pixel coordinates
+        col, row = int(col), int(row)
+
+        # assigning current ice thickness to corresponding cell in the grid
+        ice_thickness_grid[row,col] = ice_thickness[i,j]  # np.array, (row,col) convention
+        land_mask_grid[row,col] = land_mask[i,j]
+
