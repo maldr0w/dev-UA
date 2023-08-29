83138cc245d1b2dc86e853df9df6f3f984678d9b
gohermgo <gohnherman@gmail.com>
2023-08-29 16:40:05 +0200

    Added functionality for extrapolating distance correction factor based on ice thickness, as well as generating the relevant map for this

--- src/investigation.py
+++ src/investigation.py
@@ -5,6 +5,11 @@ import xarray as xr
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+def print_separator():
+    s = 68 * '='
+    print('\n<' + s + '>\n')
+
+print_separator()
 
 dataset = xr.open_dataset('ice_thickness_2022.nc')
 projdata = dataset['Lambert_Azimuthal_Grid']
@@ -29,7 +34,7 @@ _time = 0
 icedata = dataset['sea_ice_thickness'].values[_time]
 
 
-print("Shifting coordinates")
+print("Shifting coordinates...")
 def shift(c):
     return (1000 * c) + (meters / 2)
 vshift = np.vectorize(shift)
@@ -41,7 +46,9 @@ print("Finished\n")
 
 flags = dataset['status_flag'].values[_time]
 
-print("Transforming tiles...")
+print("Filtering out land cells...")
+
+# Make a new map to store all the data, including ocean tiles
 
 mapdata = np.empty(np.shape(icedata))
 
@@ -54,164 +61,143 @@ for y, row in enumerate(flags):
                 mapdata[y, x] = icedata[y, x]
 print("Finished\n")
 
+print("Generating image for the ice thickness map...")
+
+# Make a mesh corresponding to the entire map for creating a figure
+
 xx, yy = np.meshgrid(x_coords, y_coords)
+
+# Change this if needed, just a placeholder for now
 
 cmap = mpl.cm.jet
 cmap.set_bad('black',1.)
 
-# m.pcolormesh(xx, yy, mapdata, vmin=-1.0, vmax=11.0, norm='log', cmap=cmap)
+# Draw the colormesh
+
 m.pcolormesh(xx, yy, mapdata, norm='symlog', shading='nearest', antialiased=True, cmap=cmap)
 
-def colorize(f, t):
-    match f:
-        # Nominal retrieval
-        case 0:
-            # return ((t + 0.5) / 11.0, 0.11, 0.25)
-            return ([(t + 0.5) / 11.0, 0.11, 0.25])
+print("Finished\n")
 
-        # No data
-        # case 1:
-        #     return (0.0, 0.0, 0.0)
-
-        # Open ocean
-        case 2:
-            # return (0.0, 0.0, 0.5)
-            return ([0.0, 0.0, 0.5])
-
-        # Sattelite pole hole
-        # case 3:
-        #     return (0.0, 0.0, 0.0)
-        
-        # Land, lake, or land ice
-        case 4:
-            # return (0.0, 0.5, 0.0)
-            return ([0.0, 0.5, 0.0])
-
-        # Retrieval failed
-        # case 5:
-        #     return (0.0, 0.0, 0.0)
-
-        case _:
-            # return (0.0, 0.0, 0.0)
-            return ([0.0, 0.0, 0.0])
-
-print("Generating colormap")
-# colormap = np.empty(np.shape(icedata), dtype=object)
-colormap = []
-for y, flag_row in enumerate(flags):
-    # print("Iteration ", y)
-    # colorrow = np.empty(np.shape(icedata[0]))
-    row = []
-    for x, flag in enumerate(flag_row):
-        row.append(colorize(flag, icedata[y, x]))
-        # colormap[y, x] = colorize(flag, icedata[y, x])
-        # colorrow.append(colorize(flag, icedata[y, x]))
-    colormap.append(row)
-    # colormap.append(np.array(colorrow))
-    
-print("Finished")
-
-# for coord in enumerate(x_coords):
-#     print(coord)
-print("Plotting data")
-
-# for y, yc in enumerate(y_coords):
-#     m.scatter(x_coords, np.full(np.shape(y_coords), yc), c=colormap[y], marker=',', linewidths=1.0, edgecolors=None)
-
-
-# for y, yc in enumerate(y_coords):
-#     print("Iteration ", y)
-#     for x, xc in enumerate(x_coords):
-#         m.plot(xc, yc, marker=',', color=colormap[y, x])
-
-        # match flags[y, x]:
-        #     # Nominal retrieval
-        #     case 0:
-        #         thickness = icedata[y, x]
-        #         thickness_color = ((thickness + 0.5) / 11.0, 0.11, 0.25)
-        #         m.plot(xc, yc, marker=',', color=thickness_color)
-
-        #     # No data
-        #     case 1:
-        #         m.plot(xc, yc, marker=',', color=(0.0, 0.0, 0.0))
-
-        #     # Open ocean
-        #     case 2:
-        #         m.plot(xc, yc, marker=',', color='b')
-
-        #     # Sattelite pole hole
-        #     case 3:
-        #         m.plot(xc, yc, marker=',', color=(0.0, 0.0, 0.0))
-            
-        #     # Land, lake, or land ice
-        #     case 4:
-        #         m.plot(xc, yc, marker=',', color='g')
-
-        #     # Retrieval failed
-        #     case 5:
-        #         m.plot(xc, yc, marker=',', color=(0.0, 0.0, 0.0))
-
-print("Finished")
-
-
-
-
-# class Node:
-#     def __init__(self, x, y, lat, lon, ice_thickness):
-#         self.x = x
-#         self.y = y
-#         self.lat = lat
-#         self.lon = lon
-#         self.ice_thickness = ice_thickness
-#     def draw(self):
-#         if not math.isnan(self.ice_thickness):
-#             m.plot(self.x, self.y, marker=',', color=((self.ice_thickness + 0.5) / 11.0, 0.11, 0.22))
-
-
-
-
-ax = plt.gca()
-icedata = dataset['sea_ice_thickness']
-# m.pcolormesh(icedata.coords['xc'], icedata.coords['yc'], icedata.values[0])
-data_table = icedata.values[0]
-x_coords = icedata.coords['xc']
-y_coords = icedata.coords['yc']
-lat_list = icedata.coords['lat']
-lon_list = icedata.coords['lon']
-data_list = []
-# print('im gonna say im walking here')
-# for y in np.arange(0, 431, 1):
-#     data_row = data_table[y]
-#     lat_row = lat_list[y]
-#     lon_row = lon_list[y]
-#     yc = (1000 * y_coords[y]) + (meters / 2)
-#     for x in np.arange(0, 431, 1):
-#         xc = (1000 * x_coords[x]) + (meters / 2)
-#         data_list.append(Node(xc, yc, lat_row[x], lon_row[x], data_row[x]))
-
-# print("im walking here")
-
-# for data in data_list:
-#     data.draw()
-
-# def plotpoint(x, y, latlon=False):
-#     if latlon:
-#         i = 0
-#         for data in data_list:
-#             if lat - data.lat < 1 and lon - data.lon < 1:
-#                 i = data_list.index(data)
-#         xc, yc = data_list[i].x, data_list[i].y
-#         m.plot(xc, yc, marker='.')
-#     else:
-#         m.plot(x, y, marker='.')
-
-plt.title("my dick and balls")
+plt.title("Ice thickness map")
 plt.show()
-print("Saving figure")
-plt.savefig('fig.png')
-# print(icedata)
-# print(icedata.values)
-# print(icedata.coords)
+print("Saving figure...")
+plt.savefig('images/mapdatacolormesh.png')
+print("Finished")
 
-# print(dataset['Lambert_Azimuthal_Grid'])
-# print(dataset['Lambert_Azimuthal_Grid'].attrs['proj4_string'])
+print_separator()
+
+print("Generating the distance correction map...")
+# Start correcting the distance data
+
+# The following formulas are based on polynomial extrapolation of
+#   thickness 0 => 1
+#   thickness 1 => 1.2
+#   thickness 2 => 1.6
+#  
+# based on the numbers given by hadi
+
+def pn_1(x):
+    term = (x ** 2) - (3 * x) + 2
+    return term / 2
+
+def pn_2(x):
+    term = (1.2 * (x ** 2)) - (2.4 * x)
+    return (-1 * term)
+
+def pn_3(x):
+    term = (0.8 * (x ** 2)) - (0.8 * x)
+    return term
+
+def pn_of(x):
+    sum = pn_1(x) + pn_2(x) + pn_3(x)
+    return sum
+
+# print(pn_of(0), pn_of(1), pn_of(2))
+
+def ice_thickness_to_correction_factor(thickness):
+    # If the ice thickness is less than 0,
+    # it must either be ocean or something else
+    # thus just run the function clamped to 0
+    if thickness < 0:
+        return pn_of(0)
+
+    # And in any other case, just get the extrapolated data
+    else:
+        return pn_of(thickness)
+
+# Initialize a distance_correction_map based on the converted map data
+
+distance_correction_map = np.empty(np.shape(mapdata))
+
+# Iterate through the converted map data,
+
+for y, data_row in enumerate(mapdata):
+    for x, datapoint in enumerate(data_row):
+        # If the current data point is not nan, then we can
+        # convert the given ice thickness to a distance correction factor
+        if not math.isnan(datapoint):
+            distance_correction_map[y, x] = ice_thickness_to_correction_factor(datapoint)
+
+        # If the data point is nan however, then we want to keep it nan
+        # as the current implementation uses nan as land (so boats do not care)
+        else:
+            distance_correction_map[y, x] = math.nan
+
+print("Finished\n")
+
+print("Generating an image for the distance correction map...")
+
+m.pcolormesh(xx, yy, distance_correction_map, norm='symlog', shading='nearest', antialiased=True, cmap=cmap)
+
+print("Finished\n")
+
+plt.title("Distance correction map")
+plt.show()
+print("Saving figure...")
+plt.savefig('images/distancecorrectioncolormesh.png')
+print("Finished")
+
+print_separator()
+
+# def colorize(f, t):
+#     match f:
+#         # Nominal retrieval
+#         case 0:
+#             # return ((t + 0.5) / 11.0, 0.11, 0.25)
+#             return ([(t + 0.5) / 11.0, 0.11, 0.25])
+
+#         # No data
+#         # case 1:
+#         #     return (0.0, 0.0, 0.0)
+
+#         # Open ocean
+#         case 2:
+#             # return (0.0, 0.0, 0.5)
+#             return ([0.0, 0.0, 0.5])
+
+#         # Sattelite pole hole
+#         # case 3:
+#         #     return (0.0, 0.0, 0.0)
+        
+#         # Land, lake, or land ice
+#         case 4:
+#             # return (0.0, 0.5, 0.0)
+#             return ([0.0, 0.5, 0.0])
+
+#         # Retrieval failed
+#         # case 5:
+#         #     return (0.0, 0.0, 0.0)
+
+#         case _:
+#             # return (0.0, 0.0, 0.0)
+#             return ([0.0, 0.0, 0.0])
+
+# ax = plt.gca()
+# icedata = dataset['sea_ice_thickness']
+# # m.pcolormesh(icedata.coords['xc'], icedata.coords['yc'], icedata.values[0])
+# data_table = icedata.values[0]
+# x_coords = icedata.coords['xc']
+# y_coords = icedata.coords['yc']
+# lat_list = icedata.coords['lat']
+# lon_list = icedata.coords['lon']
+# data_list = []
